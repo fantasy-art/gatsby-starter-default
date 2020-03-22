0afdccc304866628aaa0a5eab50b04f491a6e793
Yahya Hamoud <fantasy-art@live.com>
2020-03-23 00:57:58 +0300

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -35,9 +35,9 @@ const Layout = ({ children }) => {
       >
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()},برمجة وتصميم 
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <a href="https://www.gatsbyjs.org">فكرة مبدع</a>
         </footer>
       </div>
     </>
