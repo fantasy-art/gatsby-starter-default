886dc694fd8a10715012f499c9627d8bed488292
Yahya Hamoud <fantasy-art@live.com>
2020-03-22 21:13:47 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,9 +8,10 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>My Name Is Anas</h1>
-    <p>I love My sister: Wanas.</p>
-    <p>Happy New Yaer.</p>
+    <h1>مسكنك للخدمات المنزلية</h1>
+    <p><a href='https://www.masknk.com'>MASKNK . COM</a></p>
+    <p>موقع يقدم افضل شركات تنظيف منازل ونقل عفش بالمملكة</p>
+    <p>نسعد بتقديم خدماتنا لكم </p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
