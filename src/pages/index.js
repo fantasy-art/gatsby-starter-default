11acfb784b2ea653cfce8735a3638bf2570b32de
Yahya Hamoud <fantasy-art@live.com>
2020-03-22 22:47:11 +0300

    New

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,10 +7,10 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Gatspy Masknk" />
-    <h1>مسكنك للخدمات المنزلية</h1>
-    <p><a href='https://www.masknk.com'>MASKNK . COM</a></p>
-    <p>موقع يقدم افضل شركات تنظيف منازل ونقل عفش بالمملكة</p>
+    <SEO title="Gatspy Motqnon" />
+    <h1>شركة متقنون للخدمات المنزلية</h1>
+
+    <p> تقدم افضل خدمات تنظيف منازل ونقل عفش بالمملكة</p>
     <p>نسعد بتقديم خدماتنا لكم </p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
