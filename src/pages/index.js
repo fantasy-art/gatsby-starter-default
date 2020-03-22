b49a2046d3aa7a3d723865b137ba4abdd3d91812
Yahya Hamoud <fantasy-art@live.com>
2020-03-22 21:28:28 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title="Gatspy Masknk" />
     <h1>مسكنك للخدمات المنزلية</h1>
     <p><a href='https://www.masknk.com'>MASKNK . COM</a></p>
     <p>موقع يقدم افضل شركات تنظيف منازل ونقل عفش بالمملكة</p>
@@ -15,7 +15,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">الصفحة التالية</Link>
   </Layout>
 )
 
