ca72a7ba7cffa6e65527287125aa422c12f31b45
Yahya Hamoud <fantasy-art@live.com>
2020-03-22 23:48:01 +0300

    Update image.js

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "motqnon.png" }) {
+      placeholderImage: file(relativePath: { eq: "Motqnon.png" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
